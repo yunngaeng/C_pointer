0973c3840b9d43c23f74d5f4a73f84524c253f4b
yunngaeng <yungyeong04@gmail.com>
2024-11-10 16:43:55 +0900

    Add files via upload

---
+++ 실습2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+	int i = 3000;
+	int* p = NULL;
+
+	p = &i;	//핵심코드
+	printf("p = %p\n", p);
+	printf("&i = %p\n\n", &i);
+	printf("i = %d\n", i);
+	printf("*p = %d\n", *p);
+
+	return 0;
+}
